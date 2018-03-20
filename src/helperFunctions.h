81f52280637192801d92a9b6ef0c1a53f628d13e
SekyHC <hrvoje.seketa@homecontrol.no>
2018-03-20 17:19:12 +0100

    moved files around

---
+++ src/helperFunctions.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "Arduino.h"
+
+void clearString(byte* text, unsigned int length);
+int lineContains(const char *str, const char *sfind);
+float extractFloat(byte* text, unsigned int length);
+int extractInteger(byte* text, unsigned int length);
+bool extractState(byte* text, unsigned int length);
+bool extractBool(byte* text, unsigned int length);
