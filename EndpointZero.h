5e9a708a79be6a1f37dd1b7015b820a11bab9455
SekyHC <hrvoje.seketa@homecontrol.no>
2018-03-20 12:51:40 +0100

    inital adding of files

---
+++ EndpointZero.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "Endpoint.h"
+
+class EndpointZero : public Endpoint
+{
+  public:
+  EndpointZero(HomeControlMagic* hcm_ptr);
+
+  void sendConfig();
+  void incomingMessage(char* topic, byte* payload, unsigned int length);    
+};
+
---
+++ HomeControlMagic.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "Endpoint.h"
+
+class HomeControlMagic
+{
+  public:
+    HomeControlMagic(char* server_ip);
+    void doMagic(bool connection_ok);
+
+    Endpoint* getEndpoint(uint8_t number);
+    void addEndpoint(Endpoint* endpoint_ptr);
+    
+    char* getId();    
+    uint8_t getNumberOfEndpoints();
+
+    void sendConfigs();
+    void announce();
+    
+    void sendMessage(String topic, String message, char* endpoint_id);
+    void sendMessage(String topic, uint8_t message, char* endpoint_id);
+
+  private:
+    bool reconnectMqtt();
+    void subscribeNow();
+    void mqttLoop(bool reconnect);
+
+    String m_name;
+
+    
+    uint8_t m_number_of_endpoints;
+    Endpoint* m_endpoints_pointers[10];    
+
+    const uint16_t m_mqtt_port = 1883;
+    char m_id[10];
+
+    int m_reconnectTime = 30000;
+    long m_lastTimeConnected = 0;
+    long m_lastReconnectAttempt = 0;
+};
+
