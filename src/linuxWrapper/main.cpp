e5ccdb40571d701beb8efda98b02275c3962e1eb
Hrvoje <hrvoje.seketa@homecontrol.no>
2018-09-23 08:30:03 -0700

    inital version
    added arrrgh
    created makefile

---
+++ src/linuxWrapper/main.cpp
@@ -0,0 +1,31 @@
+#include "stdint.h"
+#include "../HomeControlMagic.h"
+#include "lib/arrrgh/arrrgh.hpp"
+#include "stdio.h"
+
+
+int main(int argc, const char* argv[])
+{
+    arrrgh::parser parser( "hc_mqtt_client", "HomeControl MQTT client example" );
+
+    const auto& debug_mode = parser.add<bool>("debug", "Turn on debug mode", 'd', arrrgh::Optional, false);
+
+    try
+    {
+    parser.parse(argc, argv);
+    }
+    catch(...)
+    {
+        std::cerr << "Parsing failed" << std::endl;
+    }
+
+    if(debug_mode.value())
+    {
+        std::cout << "Debug mode on" << std::endl;
+    }
+
+    std::cout << "Exiting" << std::endl;
+
+    return 0;
+
+}
