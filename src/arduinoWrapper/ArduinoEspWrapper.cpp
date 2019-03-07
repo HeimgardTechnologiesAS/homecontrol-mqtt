97077159db408f00c70fec75ae921055e7c63a8b
Morgan Tørvolt <morgan@torvolt.com>
2019-03-07 09:47:22 +0100

    Change address to hex, easier to match with MAC when scanning network, and takes less space.

--- src/arduinoWrapper/ArduinoEspWrapper.cpp
+++ src/arduinoWrapper/ArduinoEspWrapper.cpp
@@ -82,7 +82,7 @@ void networkSetup()
         m_client = new WiFiClient();
     }
     // TODO: remove sprintf
-    sprintf(m_uid, "%d", ESP.getChipId());
+    sprintf(m_uid, "%lx", ESP.getChipId());
 #ifdef ESP_WRAPPER_DEBUG
     Serial.print(F("unique id: "));
     Serial.println(m_uid);
