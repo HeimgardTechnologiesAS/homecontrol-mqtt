35a046aa3f3f12f29acbaf07944985f96a718c36
Morgan Tørvolt <morgan@torvolt.com>
2019-03-07 16:37:47 +0100

    Adding support for more endpoints per device

--- src/HomeControlMagic.h
+++ src/HomeControlMagic.h
@@ -41,7 +41,7 @@ private:
     char* m_id;
 
     uint8_t m_number_of_endpoints;
-    Endpoint* m_endpoints_pointers[10];
-    char m_base_topic[20];
+    Endpoint* m_endpoints_pointers[50];
+    char m_base_topic[30];
     bool m_broker_was_connected;
 };
