ded265005c34196b1e89714cc6a2b06f532ad26c
Morgan Tørvolt <morgan@torvolt.com>
2020-11-13 21:19:28 +0100

    Adding service startup script for systemd

--- wrappers/linuxWrapper/programs/compile_on_rpi/setup.sh
+++ wrappers/linuxWrapper/programs/compile_on_rpi/setup.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
+
+# Install necessary software
 if [ -f "/etc/debian_version" ]; then
     sudo apt-get install git libmosquittopp1 libmosquittopp-dev
-fi
-git clone https://github.com/HomeControlAS/homecontrol-mqtt.git --depth=1 homecontrol-mqtt
-cd homecontrol-mqtt
-git submodule update --init
-cd ..
+sudo apt-get install git libmosquittopp1
+
+# Create a folder in home (assuming user is pi)
+cd ~
+mkdir hc-mqtt
+cd hc-mqtt
+
+# Clone the git repo
+git clone https://github.com/HomeControlAS/homecontrol-mqtt.git --depth=1 --recurse-submodules -j4 homecontrol-mqtt
+
+# Install systemd script
+sudo cp homecontrol-mqtt/wrappers/linuxWrapper/programs/compile_on_rpi/hc-mqtt.service /lib/systemd/system/
+sudo chmod 644 /lib/systemd/system/hc-mqtt.service
+sudo chown root /lib/systemd/system/hc-mqtt.service
+
+# Copy necessary files
 cp homecontrol-mqtt/wrappers/linuxWrapper/programs/compile_on_rpi/Makefile .
 cp homecontrol-mqtt/wrappers/linuxWrapper/programs/compile_on_rpi/main.cpp .
