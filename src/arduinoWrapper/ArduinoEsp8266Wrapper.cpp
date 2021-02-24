500ebac788e1deaed3b072ff8adc9e82c10f6c37
Jouni Ilmari Ikonen <jouni.ikonen@gmail.com>
2021-02-24 10:42:49 +0100

    Corrected change reason

--- src/arduinoWrapper/ArduinoEsp8266Wrapper.cpp
+++ src/arduinoWrapper/ArduinoEsp8266Wrapper.cpp
@@ -69,7 +69,8 @@ void networkSetup()
 {
 
     // if we are using that small module for ESP make sure we killoff debug led
-    // For my esp01-s (black pcb) to be able to even boot properly.  
+    // For my esp01-s (black pcb) to be able to use serial debug. 
+    // LED_BUILTIN is defined as 1 which is TX on ESP01. 
 #ifdef ARDUINO_ESP8266_ESP01
     DebugLedPinSet(-1);
 #else
